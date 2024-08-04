8a2eb5b8403416c255438a3bf06d880345e42096
aiman-munawar <aimanmunawar6@gmail.com>
2024-08-04 22:14:39 +0500

    add

---
+++ add-key/split-array-by-splice.js
@@ -0,0 +1,15 @@
+// program to split array into smaller chunks
+
+function splitIntoChunk(arr, chunk) {
+
+    while(arr.length > 0) {
+
+        let tempArray;
+        tempArray = arr.splice(0, chunk);
+        console.log(tempArray);
+    }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunk = 2;
+splitIntoChunk(array, chunk);
