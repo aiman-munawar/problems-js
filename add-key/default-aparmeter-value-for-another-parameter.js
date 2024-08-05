e59b40dec873ba32a9d496f602ea85385f02c441
aiman-munawar <aimanmunawar6@gmail.com>
2024-08-05 21:46:43 +0500

    add

---
+++ add-key/default-aparmeter-value-for-another-parameter.js
@@ -0,0 +1,11 @@
+// using previous parameter in default value expression
+
+let calculate = function(x = 15, y = x + 2) {
+    return x + y;
+}
+
+const result1 = calculate(10);
+console.log(result1);
+
+const result2 = calculate();
+console.log(result2);
